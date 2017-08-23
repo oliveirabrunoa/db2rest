f2f2e68e58a14016177eb41d1e27ded24360ffca
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-08-23 12:04:44 -0300

    Configuração para obter informação do path do banco através de variável de ambiente

--- MapToRest/db_config.py
+++ MapToRest/db_config.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine,MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-engine = create_engine('sqlite:///test.db', convert_unicode=True)
+engine = create_engine(os.environ['DATABASE_URL'], convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
