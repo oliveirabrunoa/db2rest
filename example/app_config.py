533a2af7f35c73b8ccc0e6e3ee0dc7a93644370f
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-08-24 14:30:33 -0300

    Remoção de comentários; alteração de nome da app

--- example/app_config.py
+++ example/app_config.py
@@ -7,7 +7,7 @@ from blueprints.category import category
 
 
 def create_app():
-    app = Flask("emile")
+    app = Flask("example")
 
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- example/blueprints/category.py
+++ example/blueprints/category.py
@@ -1,12 +1,6 @@
 from flask import Blueprint, jsonify, request
 import datetime
 
-# @app.route('/course_type', methods=['GET'])
-# def course_type_teste():
-#     print("teste")
-#     return True
-#     # course_types = models.Post.query.all()
-#     # return jsonify(result=[dict(id=ct.id_course_type, description=ct.description_course_type) for ct in course_types])
 category = Blueprint("category", __name__)
 
 @category.route('/category')
