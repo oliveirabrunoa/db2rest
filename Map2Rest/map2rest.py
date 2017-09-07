43021fd353af9cff2b61babc46de9ad7a664cff2
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-09-07 00:33:06 -0300

    Criação de função no ModelHelper para obter relacionamentos dos modelos

--- Map2Rest/map2rest.py
+++ Map2Rest/map2rest.py
@@ -65,15 +65,17 @@ class LoadModelClasses(object):
     #Geração do arquivo contendo os modelos.
     def generate_models(self):
         list_models = self.models_list()
+        for j in list_models:
+            j.get_relationships()
 
-        for model in list_models:
-            if self.check_model_attributes(model):
-                setattr(model, 'attributes', model.get_model_attributes())
-            else:
-                print('O __tablename__ informado para o modelo {0} não existe na base de dados.'.format(model.__modelname__))
-                exit()
-
-        render_to_template("Map2Rest/models.py", "model_template.py", list_models)
+        # for model in list_models:
+        #     if self.check_model_attributes(model):
+        #         setattr(model, 'attributes', model.get_model_attributes())
+        #     else:
+        #         print('O __tablename__ informado para o modelo {0} não existe na base de dados.'.format(model.__modelname__))
+        #         exit()
+        #
+        # render_to_template("Map2Rest/models.py", "model_template.py", list_models)
 
     #Pendente: Criação de função para identificar relações, chaves e atributos compostos de acordo com valores informados!
 
@@ -93,4 +95,12 @@ class ModelHelper(object):
 
         return model_attributes
 
-    
+    def get_relationships(self):
+        relationships_attr = getattr(self, 'relationships', None)
+        relationships = {}
+        if relationships_attr:
+            for relationship in relationships_attr:
+                fields = relationship['association_table'].split('|')
+                relationships = dict(type=relationship['type'], child=relationship['child'],
+                                association_table=[r for r in fields])
+        return relationships
